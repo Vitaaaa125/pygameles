f1b95b840fa837aec08efa5cf08bc853fd6ce7c8
Vitaaaa125 <kedvitalik@gmail.com>
2025-09-17 18:53:03 +0300

    додавли керування, клачи, зыткнення

--- le1.py
+++ le1.py
@@ -7,15 +7,50 @@ FPS = 60
  
 bg = image.load("images/background.png")
 bg = transform.scale(bg, (900, 600))
-player1 = transform.scale(image.load("images/sprite1.png"), (75, 75) )
+
+class Player(sprite.Sprite):
+    def __init__(self, sprite_image, x, y):
+        super().__init__()
+
+        self.image = transform.scale(image.load(sprite_image), (75, 75) )
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 1.5
+
+    def draw(self, window):
+        window.blit(self.image, self.rect)
+
+    def update(self, key_r, key_l, key_u, key_d):
+        keys = key.get_pressed()
+        if keys[key_r]:
+            self.rect.x += self.speed
+        if keys[key_l]:
+            self.rect.x -= self.speed
+        if keys[key_d]:
+            self.rect.y += self.speed
+        if keys[key_u]:
+            self.rect.y -= self.speed
+    
+
+
+player1 = Player("images/sprite1.png", 150, 450)
+player2 = Player("images/sprite2.png", 450, 450)
+
+
 
 while True:
     window.blit(bg, (0, 0))
     for e in event.get():
         if e.type == QUIT:
             quit()
-    window.blit(player1, (150, 150))
     
+    player1.update(K_RIGHT, K_LEFT, K_UP, K_DOWN)
+    player2.update(K_d, K_a, K_w, K_s)
 
+    if sprite.collide_rect(player1, player2):
+        quit()
+    player1.draw(window)
+    player2.draw(window)
     display.update()
     clock.tick(FPS)
